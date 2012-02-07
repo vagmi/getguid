42475749eba35fe165adb994d821951ccefdd736
vagmi <vagmi.mudumbai@gmail.com>
2012-02-07 23:18:02 +0530

    remove the unnecessary hyphens

--- getguid.rb
+++ getguid.rb
@@ -8,6 +8,6 @@ get '/guids.json' do
   count = count.to_i if (count.is_a? String) 
   count = 100 if count < 1
   count=300 if count > 300
-  guids=1.upto(count).map { Guid.new.to_s }
+  guids=1.upto(count).map { Guid.new.to_s.gsub("-","") }
   guids.to_json
 end
